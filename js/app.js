cc58ff8f3028597b0d30ff1847be52963ca01352
Pep Farin <pepfarin@gmail.com>
2023-07-21 11:19:55 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    } 
+});
